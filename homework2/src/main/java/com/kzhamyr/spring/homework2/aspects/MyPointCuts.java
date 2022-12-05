fb7778e93f53c3d18f93f10650e86873cdc35b1b
Dasha00008 <95524401+Dasha00008@users.noreply.github.com>
2022-12-05 15:58:55 +0300

    Add files via upload

--- homework2/src/main/java/com/kzhamyr/spring/homework2/aspects/MyPointCuts.java
+++ homework2/src/main/java/com/kzhamyr/spring/homework2/aspects/MyPointCuts.java
@@ -4,7 +4,8 @@ import org.aspectj.lang.annotation.Pointcut;
 
 public class MyPointCuts {
 
-    @Pointcut("execution(* com.kzhamyr.spring.homework2.UniLibrary._add*(..))")
+    @Pointcut("execution(* com.kzhamyr.spring.homework2.UniLibrary.add*(..))")
     public void allAddMethods() {
+        System.out.println("YES");
     }
 }
